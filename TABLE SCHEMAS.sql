649f132fed68dc271606fbf8a2b3f6c1688ae0a9
Hanwen Jia <123384453+J1ahw@users.noreply.github.com>
2023-03-27 14:45:38 +0100

    Update TABLE SCHEMAS.sql

--- TABLE SCHEMAS.sql
+++ TABLE SCHEMAS.sql
@@ -2,19 +2,19 @@ ALTER DATABASE employ_db SET datestyle TO "ISO, MDY";
 
 --drop table titles CASCADE;
 create table titles(
-    title_id varchar(5),
+    title_id varchar(5) NOT NULL,
     title varchar(30),
     PRIMARY KEY (title_id));
 
 --drop table departments CASCADE;
 create table departments(
-    dept_no varchar(4),
+    dept_no varchar(4) NOT NULL,
     dept_name varchar(30),
     PRIMARY KEY (dept_no));
 
 --drop table employees CASCADE;
 create table employees(
-    emp_no varchar(6),
+    emp_no varchar(6) NOT NULL,
     emp_title_id varchar(5),
     birth_date date,
     first_name varchar(30),
@@ -26,8 +26,8 @@ create table employees(
 
 --drop table dept_manager CASCADE ;
 create table dept_manager(
-    dept_no varchar(4),
-    emp_no varchar(6),
+    dept_no varchar(4) NOT NULL,
+    emp_no varchar(6) NOT NULL,
 	primary key (dept_no,emp_no),
      FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	 FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
@@ -35,8 +35,8 @@ create table dept_manager(
 
 --drop table dept_emp CASCADE ;
 create table dept_emp(
-    emp_no varchar(6),
-    dept_no varchar(4),
+    emp_no varchar(6) NOT NULL,
+    dept_no varchar(4) NOT NULL,
 	primary key (dept_no,emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
@@ -44,6 +44,6 @@ create table dept_emp(
 
 --drop table salaries CASCADE;
 create table salaries(
-    emp_no varchar(6),
+    emp_no varchar(6) NOT NULL,
     salary  int,
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
